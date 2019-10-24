ed150af08d2cf283733fdfb952f59d1be73b4ab1
xiaobobo01 <yangb@jinzantech.com>
2019-10-24 09:19:25 +0800

    日志

--- src/main/java/com/github/maskedlaodi/common/impl/FrameworkAop.java
+++ src/main/java/com/github/maskedlaodi/common/impl/FrameworkAop.java
@@ -2,12 +2,16 @@ package com.github.maskedlaodi.common.impl;
 
 
 import com.github.maskedlaodi.common.Framework;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class FrameworkAop implements Framework {
 
     private FrameworkImpl                   framework = new FrameworkImpl();
+
+    private static Logger                   logger = LoggerFactory.getLogger(FrameworkAop.class);
 
     @Override
     public void export(Map<String, Object> serviceMap, int port) {
@@ -21,7 +25,7 @@ public class FrameworkAop implements Framework {
             return framework.refer(interfaceClass, host, port);
         } finally {
             final long end = System.currentTimeMillis();
-            System.out.println(">>>>>>>>>>> " + interfaceClass.toString() + " spend time: " + (end - begin) + " <<<<<<<<<<<<<");
+            logger.info(">>>>>>>>>>> {} spend time: {} <<<<<<<<<<<<<", interfaceClass.toString(), (end - begin));
         }
     }
 }
